cc9746e2bd0d116748608b0ec6b94910023d7a3a
Aaron <aarnliebrmn@gmail.com>
2019-04-14 21:21:22 -0700

    auto commit

--- public/fiar/Mouse.js
+++ public/fiar/Mouse.js
@@ -20,7 +20,7 @@ function Mouse() {
     self.pos = self.convertTouches(e);
     let dx = self.pos[0] - self.ipos[0];
     let dy = self.pos[1] - self.ipos[1];
-    game.grid.moveAll(dx,dy)
+    //game.grid.moveAll(dx,dy)
   }
 
   this.mouseUp = function() {
