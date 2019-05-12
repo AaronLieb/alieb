15bea0a7f22f8c91574c4b144e441dcdda363582
Aaron <aarnliebrmn@gmail.com>
2019-05-12 11:17:06 -0700

    auto commit

--- public/home2/main.js
+++ public/home2/main.js
@@ -1,6 +1,18 @@
 function main() {
+  $('.scrollDown').click(function() {
+    scrollTo('.contentContainer')
+  })
 }
 
 $(document).ready(function() {
   main();
 })
+
+function scrollTo(id) {
+
+	$(document).ready(function (){
+			$('html, body').animate({
+			scrollTop: $(id).offset().top
+		}, 1300);
+	});
+}
