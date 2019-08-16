d57dcd67c73aec5121dd23c86478dba53916f6ec
Aaron <aarnliebrmn@gmail.com>
2019-08-16 10:07:56 -0700

    auto commit

--- public/baseconverter/index.js
+++ public/baseconverter/index.js
@@ -1,3 +1,7 @@
+window.onload = function() {
+    $("#number").attr("placeholder", ~~(Math.random() * 100))
+}
+
 function copyText() {
   console.log("Text Copied");
   $(".output").select()
@@ -6,17 +10,25 @@ function copyText() {
   $(".popup").show().delay(300).fadeOut(700);
 }
 
-function convert() {
-  var output = "";
-  var input = $("#number").val();
+function verifyInput() {
   const b1 = $("#base1").val();
   const b2 = $("#base2").val();
-  if (b1.toLowerCase() =="text") {
+  if ((b1,b2 >= 2 && b1,b2 <= 36) || ((b1.toLowerCase() == "text") && b2 >= 2 && b2 <=36) || ((b2.toLowerCase() == "text") && b1 >= 2 && b1 <=36)) {
+    convert(b1,b2);
+  } else {
+    $(".output").val("Error")
+  }
+}
+
+function convert(b1,b2) {
+  var output = "";
+  var input = $("#number").val();
+  if (b1.toLowerCase() == "text") {
     for (var i = 0; i < input.length; i++) {
       output += input[i].charCodeAt(0).toString(b2) + " ";
     }
     output = output.substring(0, output.length - 1);
-  } else if (b2.toLowerCase() =="text") {
+  } else if (b2.toLowerCase() == "text") {
     var binArr = input.split(" ");
     for (i in binArr) {
       output += String.fromCharCode(parseInt(binArr[i], b1))
