e00fbeb41f2c3a18fe1ff870f13348edd864b4a5
Aaron <aarnliebrmn@gmail.com>
2019-08-15 18:09:56 -0700

    auto commit

--- public/baseconverter/index.js
+++ public/baseconverter/index.js
@@ -11,17 +11,16 @@ function convert() {
   var input = $("#number").val();
   const b1 = $("#base1").val();
   const b2 = $("#base2").val();
-  if (b1=="text") {
+  if (b1.toLowerCase() =="text") {
     for (var i = 0; i < input.length; i++) {
       output += input[i].charCodeAt(0).toString(b2) + " ";
     }
     output = output.substring(0, output.length - 1);
-  } else if (b2=="text") {
+  } else if (b2.toLowerCase() =="text") {
     var binArr = input.split(" ");
     for (i in binArr) {
       output += String.fromCharCode(parseInt(binArr[i], b1))
     }
-    //var binArr = parseInt(num, b1).toString(2);
   }
   else {
     var numC = parseInt(input, b1);
